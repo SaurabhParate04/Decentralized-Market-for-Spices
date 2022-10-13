c3b359491878a6d4028034add9da4e7a0577df46
Saurabh Parate <sparate04@gmail.com>
2022-10-13 20:25:15 +0530

    Working on consumer user context and navbar

---
+++ web-app/src/context/User/UserState.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+import userContext from './userContext'
+
+const UserState = ({children}) => {
+    
+    //state used for 2 step registration / signup
+    const [globalCredentials, setglobalCredentials] = useState({email:"", username:"", password:"", rpassword:"", address:"", firstname:"", lastname:"", phoneno:"", usertype:""});
+
+    //global login state
+    const [loggedIn, setloggedIn] = useState(localStorage.getItem('PBPjwtToken') ? true :false);
+
+    //global state for storing profile info, to display userinfo and edit profile
+    const [userProfile, setuserProfile] = useState({email:"",username:"", address:"", firstname:"", lastname:"", phoneno:"", userWallet:"", usertype:""});
+
+    //gets userInfo
+    const getProfileInfo = async() =>{
+        try {
+            const url = "http://localhost:5000/api/user/fetchuser";
+
+            //generate fetch request to get user info for the profile
+            const response = await fetch(url, {
+                method: 'GET', 
+                headers: {
+                'Content-Type': 'application/json',
+                'accept':'application/json',
+                'auth-token':localStorage.getItem('PBPjwtToken')
+                }
+            });
+
+            //converting to json format
+            const json = await response.json();
+
+            //setting user profile state
+            setuserProfile(json)
+            
+        } catch (error) {
+            console.error(error.message);
+            console.log('error occured in getprofileinfo');
+        }
+    }
+
+    const logOutUser = ()=>{
+        localStorage.removeItem('PBPjwtToken');
+        setloggedIn(false);
+        setuserProfile("")
+    }
+            
+    return (
+        <userContext.Provider value={{globalCredentials, setglobalCredentials, loggedIn, setloggedIn, getProfileInfo, userProfile, setuserProfile, logOutUser}} >
+            {children}
+        </userContext.Provider>
+    )
+}
+
+export default UserState
