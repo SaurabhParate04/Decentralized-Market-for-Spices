a63382802f6dec3c5419ce5fead68e6b2a2197ff
Saurabh Parate <sparate04@gmail.com>
2022-10-13 20:23:30 +0530

    Working on products page

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation
+} from "react-router-dom";
+
+import Products from './pages/Products';
 
 function App() {
+  const useScrollToTop = () => {
+    const location = useLocation();
+    useEffect(() => {
+      window.scrollTo({ top: 0 });
+     // scroll to the top of the browser window when changing route
+     // the window object is a normal DOM object and is safe to use in React.
+    }, [location]);
+  };
+
+  //console.log(account);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserState>
+        <Router >
+          <Switch>
+            <Route exact path="/products">
+              <Products useScrollToTop={useScrollToTop} />
+            </Route>
+          </Switch>
+        </Router>
+      </UserState>
     </div>
   );
 }
 
 export default App;
+
---
+++ web-app/src/pages/Products.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState, useContext } from 'react'
+import userContext from '../context/User/userContext';
+import ProductCard from '../components/ProductCard';
+import './Products.css'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+// import Dropdown from '../components/Dropdown';
+
+const CharityZone = (props) => {
+
+    //state declaration
+    props.useScrollToTop();
+
+    // Admin Buttons Hide based on superUser
+    const context = useContext(userContext);
+    const { getProfileInfo, userProfile, loggedIn} = context;
+    const { usertype } = userProfile;
+
+    const [allCardsInfo, setAllCardsInfo] = useState([]);
+    const [categoryFilter, setcategoryFilter] = useState("ALL")
+    //eslint-disable-next-line
+
+    useEffect(() => {
+        getAllProducts();
+        getProfileInfo();
+    }, [])
+
+    const getAllProducts = async() => {
+        try {
+            const url = "http://localhost:5000/api/product/fetchallproducts"
+            const response = await fetch(url, {
+                method: 'GET', 
+                headers: {
+                    'Content-Type': 'application/json',
+                    'accept':'application/json',
+                }
+            });
+            const data = await response.json();
+            setAllCardsInfo(data)
+            //console.log(allCardsInfo)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    // const handleOnClick = (e) => {
+    //     if(e.target.getAttribute("title")) {
+    //         let x = e.pageX - 17.3 -50  ;
+    //         let y = e.pageY - 74 - 53.8 - 15;
+    //         setcoords({xcoords:x,ycoords:y})
+    //         setcategoryFilter(e.target.getAttribute("title"))
+    //     }
+    // }
+
+    const cardFilter = (category)=>{
+        if(categoryFilter === "ALL")
+        {
+            return true;
+        }
+        else{
+            if(categoryFilter === category)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    const clearFilter = ()=>{
+        setcategoryFilter("ALL");
+    }
+
+    return (
+        <>   
+            <Navbar/>
+
+            <div className="mapContainer">
+                <Map coords={coords} handleOnClick={handleOnClick}  /> 
+                <div className="mapStateContainer">
+                {/* <div className="dropdown-container align-items-center" id="dd-1">
+                    <Dropdown categoryFilter={categoryFilter}  setcategoryFilter={setcategoryFilter} />
+                </div> */}
+                <h3 className="mapState" id="map-2">{categoryFilter}</h3>
+                    <div className="mt-4 " id="filterCharityWrapper">
+                    {
+                        allCardsInfo.filter(card =>{
+                            return cardFilter(card.category);
+                        }).map((category,idx,arr) => (
+                
+                             <p key={idx} className="text-start">{" >  " + category.productName}</p>
+                        )) 
+                    }{' '}
+                      
+                    </div>
+                </div>  
+            </div>
+                <div className="filler_map"></div>
+            {/* SVG background */}
+            <div className="svg_background_2">
+                <div className="tilt_svg_2">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
+                    </svg>
+                </div>
+            </div>
+
+            {/* section 2 */}
+            <section className="card-conatiner-warpper">
+                <div className="cards-container-title top-0">
+                    Philanthropy zone
+                </div>
+                <button className="clear-btn top-0 end-0" onClick={clearFilter}> clear filter</button>
+                <div className=" row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mx-0 justify-content-evenly card-container gx-5">
+                    {
+                        allCardsInfo.filter(card =>{
+                            return cardFilter(card.category);
+                        }).map(card => (
+                            <ProductCard
+                                key={card._id + "5"}
+                                id={card._id}
+                                title={card.productName}
+                                description={card.description}
+                                price={card.price}
+                                qrcode={card.qrcode}
+                            />
+                        ))
+                    }{' '}
+                </div>
+            </section>
+
+            <Footer/>
+        </>
+    )
+}
+
+export default CharityZone
