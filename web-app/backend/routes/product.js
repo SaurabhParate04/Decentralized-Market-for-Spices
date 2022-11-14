d50aeb84fdbc3ec558aa1e7436edc5204c1659e9
Saurabh Parate <sparate04@gmail.com>
2022-11-14 12:51:08 +0530

    Added update and delete routes for product

--- web-app/backend/routes/product.js
+++ web-app/backend/routes/product.js
@@ -25,11 +25,41 @@ router.get('/fetchproduct', async(req, res) => {
 //createproduct post
 router.post('/createproduct', async(req, res) => {
     try {
-        product = new Product(req.body)
+        let product = new Product(req.body)
         const saved = await product.save()
         console.log(saved)
         res.json(saved)
         
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+//updateproduct put
+router.put('/updateproduct/:id', async(req, res) => {
+    try {
+        let product = await Product.findById(req.params.id)
+        if(!product) {
+            res.status(400).send('Bad request, no such product exists')
+        } else {
+            product = await Product.findByIdAndUpdate(req.params.id, req.body)
+            res.send("Updated the product information")
+        }
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+//deletecharity delete
+router.delete('/deleteproduct/:id', async(req, res) => {
+    try {
+        let product = await Product.findById(req.params.id)
+        if(!product) {
+            res.status(400).send('Bad request, no such product exists')
+        } else {
+            product = await Product.findByIdAndDelete(req.params.id)
+            res.send("Product deleted successfully")
+        }
     } catch(err) {
         console.log(err)
     }
