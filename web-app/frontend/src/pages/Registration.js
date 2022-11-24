65ff01593238a44a1b85be697134f30b3e2cc9a2
Gitesh Patil <patilgitesh29@gmail.com>
2022-11-24 13:45:34 +0530

    background for registration changed

--- web-app/frontend/src/pages/Registration.js
+++ web-app/frontend/src/pages/Registration.js
@@ -10,7 +10,7 @@ function Registration() {
     const context = useContext(userContext);
     const { globalCredentials } = context;
 
-    const [credentialSignUp, setcredentialSignUp] = useState({ email: globalCredentials.email, username: globalCredentials.username, password: globalCredentials.password, addressl1: "", addressl2: "", firstname: "", lastname: "", phoneNumber: "", landmark: "", pincode: ""});
+    const [credentialSignUp, setcredentialSignUp] = useState({ email: globalCredentials.email, username: globalCredentials.username, password: globalCredentials.password, addressl1: "", addressl2: "", firstname: "", lastname: "", phoneNumber: "", landmark: "", pincode: "" });
 
     useEffect(() => {
         console.log(credentialSignUp);
@@ -30,7 +30,7 @@ function Registration() {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
-                        
+
                     },
                     body: JSON.stringify({
                         email: credentialSignUp.email,
@@ -45,7 +45,7 @@ function Registration() {
                         pincode: credentialSignUp.pincode,
                     })
                 });
-                const testjson = await response.json();
+            const testjson = await response.json();
             // let imgRef = ref(firebaseStorage, `profile/${credentialSignUp.username}`);
             // uploadBytes(imgRef, profileImageReg).then(() => {
             //     console.log('image Uploaded!');
@@ -64,59 +64,61 @@ function Registration() {
     return (
         <div>
             <div className="reg">
-                {/* <Navbar /> */}
-                <div className="container_reg light-bg">
-                    <div className="title_reg">Registration</div>
-                    <div className="content_reg">
-                        <form  onSubmit={handleReg_2} onChange={onChangeSignUp}>
-                            <div className="user-details_reg">
-                                <div className="input-box_reg">
-                                    <span className="details_reg">First Name</span>
-                                    <input type="text" name="firstname" placeholder="Enter your First name"/>
+                <div className='overlay m-0 p-0'>
+                    {/* <Navbar /> */}
+                    <div className="container_reg light-bg">
+                        <div className="title_reg">Registration</div>
+                        <div className="content_reg">
+                            <form onSubmit={handleReg_2} onChange={onChangeSignUp}>
+                                <div className="user-details_reg">
+                                    <div className="input-box_reg">
+                                        <span className="details_reg">First Name</span>
+                                        <input type="text" name="firstname" placeholder="Enter your First name" />
+                                    </div>
+
+                                    <div className="input-box_reg">
+                                        <span className="details_reg">Last name</span>
+                                        <input type="text" name="lastname" placeholder="Enter your Last name" required />
+                                    </div>
+                                    <div className="input-box_reg">
+                                        <span className="details">Username</span>
+                                        <input type="text" name="username" placeholder="Enter Username" required />
+                                    </div>
+                                    <div className="input-box_reg">
+                                        <span className="details">Password</span>
+                                        <input type="text" name="password" placeholder="Enter Password" required />
+                                    </div>
+                                    <div className="input-box_reg">
+                                        <span className="details">Email</span>
+                                        <input type="text" name="email" placeholder="Enter Email" required />
+                                    </div>
+                                    <div className="input-box_reg">
+                                        <span className="details">Phone Number</span>
+                                        <input type="text" name="phoneNumber" placeholder="Enter your Phone Number" required />
+                                    </div>
+                                    <div className="input-box_reg" style={{ width: "100%" }}>
+                                        <span className="details">Address</span>
+                                        <input type="text" name="addressl1" placeholder="Enter your Address" required />
+                                    </div>
+                                    <div className="input-box_reg" style={{ width: "100%" }}>
+                                        <input type="text" name="addressl2" placeholder="Enter your Address" />
+                                    </div>
+
+                                    <div className="input-box_reg">
+                                        <span className="details">Landmark</span>
+                                        <input type="text" name="landmark" placeholder="Enter Landmark" required />
+                                    </div>
+                                    <div className="input-box_reg">
+                                        <span className="details">Pincode</span>
+                                        <input type="number" name="pincode" placeholder="Enter Pincode" required />
+                                    </div>
                                 </div>
 
-                                <div className="input-box_reg">
-                                    <span className="details_reg">Last name</span>
-                                    <input type="text" name="lastname" placeholder="Enter your Last name" required />
+                                <div className="button">
+                                    <input type="submit" value="Register" />
                                 </div>
-                                <div className="input-box_reg">
-                                    <span className="details">Username</span>
-                                    <input type="text" name="username" placeholder="Enter Username" required />
-                                </div>
-                                <div className="input-box_reg">
-                                    <span className="details">Password</span>
-                                    <input type="text" name="password" placeholder="Enter Password" required />
-                                </div>
-                                <div className="input-box_reg">
-                                    <span className="details">Email</span>
-                                    <input type="text" name="email" placeholder="Enter Email" required />
-                                </div>
-                                <div className="input-box_reg">
-                                    <span className="details">Phone Number</span>
-                                    <input type="text" name="phoneNumber" placeholder="Enter your Phone Number" required />
-                                </div>
-                                <div className="input-box_reg" style={{width : "100%"}}>
-                                    <span className="details">Address</span>
-                                    <input type="text" name="addressl1" placeholder="Enter your Address" required />
-                                </div>
-                                <div className="input-box_reg" style={{width : "100%"}}>
-                                    <input type="text" name="addressl2" placeholder="Enter your Address" />
-                                </div>
-                                
-                                <div className="input-box_reg">
-                                    <span className="details">Landmark</span>
-                                    <input type="text" name="landmark" placeholder="Enter Landmark" required />
-                                </div>
-                                <div className="input-box_reg">
-                                    <span className="details">Pincode</span>
-                                    <input type="number" name="pincode" placeholder="Enter Pincode" required />
-                                </div>
-                            </div>
-
-                            <div className="button">
-                                <input type="submit" value="Register" />
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
